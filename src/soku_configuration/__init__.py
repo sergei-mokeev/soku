c396b6dfba3938f6039bacfb429fde343a37184f
Sergey Mokeyev <sergey.mokeyev@gmail.com>
2022-09-15 22:58:31 +0300

    save

--- src/soku_configuration/__init__.py
+++ src/soku_configuration/__init__.py
@@ -4,38 +4,39 @@ from argparse import ArgumentParser
 
 class Configuration:
     def __init__(self) -> None:
-        parser = ArgumentParser()
-        for key, _cls in self.__annotations__.items():
-            value = None
-            try:
-                value = self.__getattribute__(key)
+        if hasattr(self, '__annotations__'):
+            parser = ArgumentParser()
+            for key, _cls in self.__annotations__.items():
+                value = None
+                try:
+                    value = self.__getattribute__(key)
 
-            except AttributeError:
-                pass
+                except AttributeError:
+                    pass
 
-            parser.add_argument(f'--{key}', type=_cls, default=None)
-            args = parser.parse_args()
+                parser.add_argument(f'--{key}', type=_cls, default=None)
+                args = parser.parse_args()
 
-            if args.__dict__.get(key):
-                value = args.__dict__[key]
+                if args.__dict__.get(key):
+                    value = args.__dict__[key]
 
-            if environ.get(key):
-                value = environ[key]
+                if environ.get(key):
+                    value = environ[key]
 
-            if value is None:
-                raise ValueError(f'Can not set configuration parameters {key}')
+                if value is None:
+                    raise ValueError(f'Can not set configuration parameters {key}')
 
-            if issubclass(_cls, list):
-                if isinstance(value, str):
-                    value = value.split(',')
+                if issubclass(_cls, list):
+                    if isinstance(value, str):
+                        value = value.split(',')
 
-                else:
-                    _value = []
-                    for item in value:
-                        if not isinstance(item, str):
-                            raise ValueError(f'List value must have only string')
-                        _value.append(item)
+                    else:
+                        _value = []
+                        for item in value:
+                            if not isinstance(item, str):
+                                raise ValueError(f'List value must have only string')
+                            _value.append(item)
 
-                    value = _value
+                        value = _value
 
-            self.__setattr__(key, _cls(value))
+                self.__setattr__(key, _cls(value))
--- src/soku_service/__init__.py
+++ src/soku_service/__init__.py
@@ -1,2 +1,69 @@
+import asyncio
+import logging
+from soku_configuration import Configuration
+from aiohttp.web import Application, AppRunner, TCPSite
+
+
 class Service:
-    pass
+    logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO)
+
+    def __init__(self, config: Configuration) -> None:
+        self.__loop = asyncio.get_event_loop()
+        self.__config = config
+        self.__logger = logging.getLogger()
+        self.__app = None
+
+    @property
+    def loop(self) -> asyncio.AbstractEventLoop:
+        return self.__loop
+
+    @property
+    def config(self) -> Configuration:
+        return self.__config
+
+    @property
+    def logger(self) -> logging.Logger:
+        return self.__logger
+
+    @property
+    def app(self):
+        return self.__app
+
+    async def __start_aiohttp_app(self) -> None:
+        if self.app:
+            host, port = '0.0.0.0', 8080
+            if hasattr(self.config, 'HOST'):
+                host = self.config.HOST
+
+            if hasattr(self.config, 'PORT'):
+                port = self.config.PORT
+
+            runner = AppRunner(self.app)
+            await runner.setup()
+
+            site = TCPSite(runner=runner, host=host, port=port)
+            await site.start()
+
+            self.logger.info(f'Web interface start on {host}:{port}')
+
+    def run(self) -> None:
+        self.logger.info('Service is started')
+        self.loop.create_task(self.__start_aiohttp_app())
+        self.loop.run_forever()
+
+    def set_aiohttp_app(self, app: Application) -> None:
+        self.__app = app
+
+
+if __name__ == '__main__':
+    from aiohttp.web import json_response
+
+    async def test(_):
+        return json_response({'test': 1})
+
+    s = Service(Configuration())
+    a = Application()
+    a.router.add_route('GET', '/', test)
+    s.set_aiohttp_app(a)
+    s.run()
+
